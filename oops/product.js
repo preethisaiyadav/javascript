7002b9bf26538930ea50cf2bb1f9bedaa6ce3a01
preethisaiyadav <dpreethi703@gmail.com>
2022-07-19 12:00:03 +0530

    done

---
+++ oops/product.js
@@ -0,0 +1,14 @@
+class product{
+    p_ID="p101";
+    p_name="phone5s"
+
+    get_product(){
+        return "hellow..."
+    }
+        
+    
+}
+let p1=new product()
+console.log(p1.p_ID)
+console.log(p1.p_name)
+console.log(p1.get_product())
